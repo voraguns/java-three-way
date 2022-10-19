102374324af6c81e3650f4e5e0f2eba1f796361b
Voragun <voragun2543@gmail.com>
2022-10-19 21:13:27 +0700

    sequential

--- code/Start.java
+++ code/Start.java
@@ -1,7 +1,14 @@
 class Start {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Printer p = new Printer();
         p.start();      // working in another thread
+        p.join();
+        
+        Writer w = new Writer();
+        Thread t = new Thread(w);
+        t.start();
+        t.join();
+        
         for (int i = 0; i < 1000; i++) {
             System.out.print("A");
         }
