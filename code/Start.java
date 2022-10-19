bd42c6dc9be436cbbd61715b8df68dce841e653e
Voragun <voragun2543@gmail.com>
2022-10-19 19:25:22 +0700

    count instance

--- code/Start.java
+++ code/Start.java
@@ -1,7 +1,23 @@
 class Start {
     public static void main(String[] args) {
-        int cmp = "Latte".compareTo("Cappucino");
-        System.out.println(cmp);
+        Car c = new Car();
+        c.weight = 1572.0;
+        c.serial = "GHSK-523566";
+        System.out.println(c instanceof Car);    // true
+        System.out.println(c instanceof Vehicle);    // true
+        
+        int count = 0;
+        
+        Vehicle[] a = { new Car(), new Truck(), new Car(),
+                        new Car(), new Car(), new Truck(),
+                        new Car(), new Truck(), new Truck()
+                       };
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] instanceof Car) {
+                count++;
+            }
+        }
+        System.out.println("Count : " + count);
     }
 }
 
