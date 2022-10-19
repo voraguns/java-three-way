7208cfc5ececd291c7441208899bf93941d0a889
Voragun <voragun2543@gmail.com>
2022-10-19 20:55:01 +0700

    Example Multithread

--- code/Start.java
+++ code/Start.java
@@ -1,40 +1,18 @@
-
-import java.util.Arrays;
-import java.util.Comparator;
-
 class Start {
     public static void main(String[] args) {
-        
-        Player[] a = { new Player("David", 7),
-                       new Player("Michael", 10),
-                       new Player("Frank", 8),
-                       new Player("Stephen", 4),
-                       new Player("John", 2)
-                     };
-        
-        Arrays.sort(a, (p, q) -> p.number - q.number);
-        for(int i = 0; i < a.length; i++) {
-            System.out.println(" " + a[i].name + " " + a[i].number);
+        Printer p = new Printer();
+        p.start();      // working in another thread
+        for (int i = 0; i < 1000; i++) {
+            System.out.print("A");
         }
     }
 }
 
-//class PlayerComparator implements Comparator {
-//    public int compare(Object a, Object b) {
-//        Player p = (Player) a;
-//        Player q = (Player) b;
-//        return p.number - q.number;
-//    }
-//}
-
-class Player {
-    int number;
-    String name;
-    
-    Player() { }
-    
-    Player(String name, int number) {
-        this.name = name;
-        this.number = number;
+class Printer extends Thread {
+    public void run() {
+        for(int i = 0; i < 1000; i++) {
+            System.out.print("P");
+        }
     }
 }
+
